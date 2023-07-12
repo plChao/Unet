fcd25c169319558020a27564d7ea66c3a35979fe
plchao <plchao@gmail.com>
2023-07-12 12:46:08 +0800

    clear output fix post function

--- 3_import_function/post_proc.py
+++ 3_import_function/post_proc.py
@@ -1,19 +1,20 @@
 import numpy as np
 import cv2
 
-def drop_outlyers_cont(contours, CEJ_mask, vis_process_mask, img_size):
+def drop_outlyers_cont(contours, CEJ_mask, vis_process_mask, img_size, thresh=400):
     choose_contours = []
     y_cord = np.nonzero(CEJ_mask)[0]
     y_mean, y_std = np.mean(y_cord), np.std(y_cord)
-    cv2.line(vis_process_mask, (0, int(y_mean + 2*y_std)), (img_size, int(y_mean + 2*y_std)), (0, 0, 255), 2)
-    cv2.line(vis_process_mask, (0, int(y_mean - 2*y_std)), (img_size, int(y_mean - 2*y_std)), (0, 0, 255), 2)
+    cv2.line(vis_process_mask, (0, int(y_mean + thresh)), (img_size, int(y_mean + thresh)), (0, 0, 255), 2)
+    cv2.line(vis_process_mask, (0, int(y_mean - thresh)), (img_size, int(y_mean - thresh)), (0, 0, 255), 2)
     for cont in contours:
-        flag = True
-        for pt in cont[0]:
-            if pt[1] > y_mean + 2*y_std or pt[1] < y_mean - 2*y_std:
-                cv2.drawContours(vis_process_mask, [cont], -1, (255, 0, 0), cv2.FILLED)
-                flag = False
-                break
-        if flag:
+        cnt = 0
+        s_cont = np.squeeze(cont, axis=1)
+        for pt in s_cont:
+            if pt[1] > y_mean + thresh or pt[1] < y_mean - thresh:
+                cnt += 1
+        if cnt / len(s_cont) <= 0.5:
             choose_contours.append(cont)
+        else:
+            cv2.drawContours(vis_process_mask, [cont], -1, (0, 0, 255), cv2.FILLED)
     return choose_contours
